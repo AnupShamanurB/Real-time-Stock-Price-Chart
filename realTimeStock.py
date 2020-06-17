ccad804fbe46abcfa0daf58d733f89a5ffc77178
anupshamanurb <ashaman1@asu.edu>
2020-06-17 11:49:10 -0700

    initial commit

---
+++ realTimeStock.py
@@ -0,0 +1,79 @@
+import json
+import random
+import time
+import requests
+from datetime import datetime
+
+from flask import Flask, Response, render_template, request
+
+application = Flask(__name__)
+random.seed()  # Initialize the random number generator
+
+stocks = []
+urls = []
+url_pre = "https://sandbox.iexapis.com/stable/stock/"
+url_post = "/price?token=Tsk_df26d04c4e6d418eb1f0fcb7faf953c8"
+stock_chart_data = ""
+
+@application.route('/show/<stock>', methods=["POST","GET"])
+def showStock(stock):
+    if stock == "all":
+        return render_template('base.html',stocks=stocks,chartType="bar")
+    global stock_chart_data
+    stock_chart_data = stock
+    return render_template('base.html',stocks=stocks,chartType="line")
+
+@application.route('/delete/<stock>', methods=["POST","GET"])
+def deleteStock(stock):
+    stocks.remove(stock)
+    urls.remove(url_pre+stock+url_post)
+    if stocks:
+        return render_template('base.html',stocks=stocks,chartType="bar")
+    else:
+        return render_template('base.html',chartType="")
+
+@application.route('/', methods=["POST","GET"])
+def index():
+    global stocks
+    global urls
+    if request.method == "POST":
+        stock = request.form["stock"]
+        if stock not in stocks:
+            stocks.append(stock)
+            urls.append(url_pre+stock+url_post)
+        return render_template('base.html',stocks=stocks, chartType="bar")
+    else:
+        stocks = []
+        urls = []
+        return render_template('base.html', chartType="")
+
+@application.route('/chart-data')
+def chart_data():
+    def generate_random_data():
+        while True:
+            if stock_chart_data != "":
+                json_data = json.dumps(
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': requests.get(url_pre+stock_chart_data+url_post).json()})
+                yield f"data:{json_data}\n\n"
+                time.sleep(5)
+            else:
+                return ""
+
+    return Response(generate_random_data(), mimetype='text/event-stream')
+
+@application.route('/bar-data')
+def bar_data():
+    def tester(url):
+        return requests.get(url).json()
+
+    def generate_random_data():
+        while True:
+            json_data = json.dumps(
+                {'stocks': list(stocks), 'value': list(map(tester, urls))})
+            yield f"data:{json_data}\n\n"
+            time.sleep(5)
+
+    return Response(generate_random_data(), mimetype='text/event-stream')
+
+if __name__ == '__main__':
+    application.run(debug=True, threaded=True)
